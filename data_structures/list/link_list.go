63eb15ee27271b547642325465d77f1a4c29805b
Zubin Cheung <zubin@zubin-pc.local>
2018-06-21 16:32:44 +0800

    新增单链表

--- data_structures/list/link_list.go
+++ data_structures/list/link_list.go
@@ -4,7 +4,6 @@ package list
 
 import (
 	"fmt"
-	"strconv"
 )
 
 type Node struct {
@@ -25,21 +24,6 @@ func NewNode(data interface{}, next *Node) *Node {
 // 初始化一个线性表
 func NewLinkList() *LinkList {
 	return &LinkList{}
-}
-
-// 初始化一个线性表
-func NewLinkList1() *LinkList {
-	list := NewLinkList()
-	l := list.head
-
-	for index := 0; index < 10; index++ {
-		l = NewNode("元素"+strconv.Itoa(index), nil)
-		l = l.next
-	}
-
-	list.length = 10
-
-	return list
 }
 
 // 线性表是否为空
@@ -72,11 +56,11 @@ func (list *LinkList) Size() int {
 
 // 返回线性表中第i个元素
 func (list *LinkList) GetElem(index int) (interface{}, error) {
-	if list.ListEmpty() {
+	if list.length <= 0 {
 		return nil, fmt.Errorf("list is empty")
 	}
 
-	if index < 0 || index > list.Size() {
+	if index < 1 || index > list.length {
 		return nil, fmt.Errorf("invalid index value %d", index)
 	}
 
@@ -87,6 +71,131 @@ func (list *LinkList) GetElem(index int) (interface{}, error) {
 		e = l.data
 		l = l.next
 	}
+
+	return e, nil
+}
+
+// 线性表中查找元素e
+// 返回在线性表中第1个满足关系的数据元素的位序，如果不存在返回0
+func (list *LinkList) LocateElem(e interface{}) (int, error) {
+
+	if list.length <= 0 {
+		return 0, fmt.Errorf("list is empty")
+	}
+
+	index := 1
+	l := list.head
+
+	for l != nil {
+		if l.data == e {
+			return index, nil
+		}
+
+		index++
+		l = l.next
+	}
+
+	return 0, nil
+}
+
+// 在第一个结点插入元素e
+func (list *LinkList) AddToFirst(e interface{}) error {
+	list.head = NewNode(e, list.head)
+	list.length++
+	return nil
+}
+
+// 在最后一个结点插入元素e
+func (list *LinkList) AddToLast(e interface{}) error {
+	if list.ListEmpty() {
+		// 线性表为空插入第一个结点
+		return list.AddToFirst(e)
+	}
+
+	// 查找最后一个节点
+	l := list.head
+	for l.next != nil {
+		l = l.next
+	}
+
+	l.next = NewNode(e, nil)
+	list.length++
+	return nil
+}
+
+// 在线性表第i个位置之前插入元素e
+func (list *LinkList) Insert(i int, e interface{}) error {
+	if i < 1 || i > list.length+1 {
+		return fmt.Errorf("invalid index value %d", i)
+	}
+
+	if i == 1 {
+		// 插入第一个结点
+		return list.AddToFirst(e)
+	} else if i == list.length+1 {
+		// 插入最后一个节点
+		return list.AddToLast(e)
+	}
+
+	l := list.head
+	j := 1
+
+	// 查找第i个结点
+	for j < i {
+		l = l.next
+		j++
+	}
+
+	node := NewNode(l.data, l.next) // 把新节点的后继指向l的后继节点，
+
+	l.data = e
+	l.next = node //l的后继节点指向新节点
+
+	list.length++
+	return nil
+
+}
+
+// 移除第一个结点
+func (list *LinkList) RemoveFirst() (interface{}, error) {
+	if list.ListEmpty() {
+		return nil, fmt.Errorf("list empty")
+	}
+
+	e := list.head.data
+
+	list.head = list.head.next
+	list.length--
+	return e, nil
+}
+
+// 移除最后一个结点
+func (list *LinkList) RemoveLast() (interface{}, error) {
+	return list.Remove(list.length)
+}
+
+// 删除第i个元素
+func (list *LinkList) Remove(i int) (interface{}, error) {
+	if list.ListEmpty() {
+		return nil, fmt.Errorf("list empty")
+	}
+	if i > list.length {
+		return nil, fmt.Errorf("invalid index value %d", i)
+	}
+
+	if i == 1 {
+		return list.RemoveFirst()
+	}
+
+	// 查找待删除元素的上一个结点
+	l := list.head
+	for j := 1; j < i-1; j++ {
+		l = l.next
+	}
+
+	e := l.next.data
+	l.next = l.next.next
+	list.length--
 
 	return e, nil
 }
--- data_structures/list/link_list_test.go
+++ data_structures/list/link_list_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 )
 
+type check func(list *LinkList) bool
+
 func TestNewLinkList(t *testing.T) {
 	tests := []struct {
 		name string
@@ -46,6 +48,509 @@ func TestNewNode(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := NewNode(tt.args.data, tt.args.next); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLinkList_ListEmpty(t *testing.T) {
+	type fields struct {
+		head   *Node
+		length int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		// TODO: Add test cases.
+		{
+			fields: fields{},
+			want:   true,
+		},
+		{
+			fields: fields{head: NewNode("node1", nil), length: 1},
+			want:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := &LinkList{
+				head:   tt.fields.head,
+				length: tt.fields.length,
+			}
+			if got := list.ListEmpty(); got != tt.want {
+				t.Errorf("LinkList.ListEmpty() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLinkList_ClearList(t *testing.T) {
+	type fields struct {
+		head   *Node
+		length int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		// TODO: Add test cases.
+		{
+			fields: fields{},
+		},
+		{
+			fields: fields{head: NewNode("node1", nil), length: 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := &LinkList{
+				head:   tt.fields.head,
+				length: tt.fields.length,
+			}
+			list.ClearList()
+			if !list.ListEmpty() {
+				t.Error("list  not empty")
+			}
+		})
+	}
+}
+
+func TestLinkList_Size(t *testing.T) {
+	type fields struct {
+		head   *Node
+		length int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		{
+			fields: fields{},
+			want:   0,
+		},
+		{
+			fields: fields{head: NewNode("node1", nil), length: 1},
+			want:   1,
+		},
+		{
+			fields: fields{head: NewNode("node1", NewNode("node2", NewNode("node3", nil))), length: 3},
+			want:   3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := &LinkList{
+				head:   tt.fields.head,
+				length: tt.fields.length,
+			}
+			if got := list.Size(); got != tt.want {
+				t.Errorf("LinkList.Size() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLinkList_GetElem(t *testing.T) {
+	type fields struct {
+		head   *Node
+		length int
+	}
+	type args struct {
+		index int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    interface{}
+		wantErr bool
+	}{
+		{
+			fields:  fields{},
+			args:    args{index: 2},
+			wantErr: true,
+		},
+		{
+			fields:  fields{head: NewNode("node1", nil), length: 1},
+			args:    args{index: 2},
+			wantErr: true,
+		},
+		{
+			fields:  fields{head: NewNode("node1", NewNode("node2", NewNode("node3", nil))), length: 3},
+			args:    args{index: 2},
+			wantErr: false,
+			want:    "node2",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := &LinkList{
+				head:   tt.fields.head,
+				length: tt.fields.length,
+			}
+			got, err := list.GetElem(tt.args.index)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LinkList.GetElem() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LinkList.GetElem() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLinkList_LocateElem(t *testing.T) {
+	type fields struct {
+		head   *Node
+		length int
+	}
+	type args struct {
+		e interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    int
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			fields:  fields{},
+			args:    args{e: 2},
+			wantErr: true,
+		},
+		{
+			fields:  fields{head: NewNode("node1", nil), length: 1},
+			args:    args{e: "node 2"},
+			wantErr: false,
+			want:    0,
+		},
+		{
+			fields:  fields{head: NewNode("node1", NewNode("node2", NewNode("node3", nil))), length: 3},
+			args:    args{e: "node2"},
+			wantErr: false,
+			want:    2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := &LinkList{
+				head:   tt.fields.head,
+				length: tt.fields.length,
+			}
+			got, err := list.LocateElem(tt.args.e)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LinkList.LocateElem() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("LinkList.LocateElem() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLinkList_Insert(t *testing.T) {
+	type fields struct {
+		head   *Node
+		length int
+	}
+
+	type args struct {
+		i int
+		e interface{}
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		wantErr   bool
+		checkFunc check
+	}{
+		// TODO: Add test cases.
+		{
+			fields:  fields{},
+			args:    args{e: "node1", i: 2},
+			wantErr: true,
+		},
+		{
+			fields:  fields{head: NewNode("node1", nil), length: 1},
+			args:    args{e: "node 2", i: 3},
+			wantErr: true,
+		},
+		{
+			fields:  fields{head: NewNode("node1", nil), length: 1},
+			args:    args{e: "node2", i: 1},
+			wantErr: false,
+			checkFunc: func(list *LinkList) bool {
+
+				if data, _ := list.GetElem(1); data != "node2" {
+					return false
+				}
+
+				if data, _ := list.GetElem(2); data != "node1" {
+					return false
+				}
+
+				if list.Size() != 2 {
+					return false
+				}
+
+				return true
+			},
+		},
+
+		{
+			fields:  fields{head: NewNode("node1", NewNode("node2", nil)), length: 2},
+			args:    args{e: "node3", i: 3},
+			wantErr: false,
+			checkFunc: func(list *LinkList) bool {
+
+				if data, _ := list.GetElem(2); data != "node2" {
+					return false
+				}
+
+				if data, _ := list.GetElem(3); data != "node3" {
+					return false
+				}
+
+				if list.Size() != 3 {
+					return false
+				}
+
+				return true
+			},
+		},
+
+		{
+			fields:  fields{head: NewNode("node1", NewNode("node2", nil)), length: 2},
+			args:    args{e: "node3", i: 2},
+			wantErr: false,
+			checkFunc: func(list *LinkList) bool {
+
+				if data, _ := list.GetElem(3); data != "node2" {
+					return false
+				}
+
+				if data, _ := list.GetElem(2); data != "node3" {
+					return false
+				}
+
+				if list.Size() != 3 {
+					return false
+				}
+
+				return true
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := &LinkList{
+				head:   tt.fields.head,
+				length: tt.fields.length,
+			}
+			if err := list.Insert(tt.args.i, tt.args.e); (err != nil) != tt.wantErr {
+				t.Errorf("LinkList.Insert() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if tt.checkFunc != nil && !tt.checkFunc(list) {
+				t.Error("insert error")
+			}
+		})
+	}
+}
+
+func TestLinkList_AddToLast(t *testing.T) {
+	type fields struct {
+		head   *Node
+		length int
+	}
+	type args struct {
+		e interface{}
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		wantErr   bool
+		checkFunc check
+	}{
+		// TODO: Add test cases.
+		{
+			fields:  fields{},
+			args:    args{e: "node2"},
+			wantErr: false,
+			checkFunc: func(list *LinkList) bool {
+
+				if data, _ := list.GetElem(1); data != "node2" {
+					return false
+				}
+
+				if list.Size() != 1 {
+					return false
+				}
+
+				return true
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := &LinkList{
+				head:   tt.fields.head,
+				length: tt.fields.length,
+			}
+			if err := list.AddToLast(tt.args.e); (err != nil) != tt.wantErr {
+				t.Errorf("LinkList.AddToLast() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestLinkList_RemoveFirst(t *testing.T) {
+	type fields struct {
+		head   *Node
+		length int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    interface{}
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			fields:  fields{},
+			wantErr: true,
+		},
+		{
+			fields:  fields{head: NewNode("node1", NewNode("node2", nil)), length: 2},
+			wantErr: false,
+			want:    "node1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := &LinkList{
+				head:   tt.fields.head,
+				length: tt.fields.length,
+			}
+			got, err := list.RemoveFirst()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LinkList.RemoveFirst() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LinkList.RemoveFirst() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLinkList_RemoveLast(t *testing.T) {
+	type fields struct {
+		head   *Node
+		length int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    interface{}
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			fields:  fields{},
+			wantErr: true,
+		},
+		{
+			fields:  fields{head: NewNode("node1", NewNode("node2", nil)), length: 2},
+			wantErr: false,
+			want:    "node2",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := &LinkList{
+				head:   tt.fields.head,
+				length: tt.fields.length,
+			}
+			got, err := list.RemoveLast()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LinkList.RemoveLast() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LinkList.RemoveLast() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLinkList_Remove(t *testing.T) {
+	type fields struct {
+		head   *Node
+		length int
+	}
+	type args struct {
+		i int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    interface{}
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			fields:  fields{},
+			wantErr: true,
+		},
+
+		{
+			fields:  fields{head: NewNode("node1", NewNode("node2", nil)), length: 2},
+			wantErr: true,
+			args:    args{i: 13},
+		},
+		{
+			fields:  fields{head: NewNode("node1", NewNode("node2", nil)), length: 2},
+			wantErr: false,
+			args:    args{i: 1},
+			want:    "node1",
+		},
+		{
+			fields:  fields{head: NewNode("node1", NewNode("node2", nil)), length: 2},
+			wantErr: false,
+			args:    args{i: 2},
+			want:    "node2",
+		},
+		{
+			fields:  fields{head: NewNode("node1", NewNode("node2", NewNode("node3", NewNode("node4", nil)))), length: 4},
+			wantErr: false,
+			args:    args{i: 3},
+			want:    "node3",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := &LinkList{
+				head:   tt.fields.head,
+				length: tt.fields.length,
+			}
+			got, err := list.Remove(tt.args.i)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LinkList.Remove() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LinkList.Remove() = %v, want %v", got, tt.want)
 			}
 		})
 	}

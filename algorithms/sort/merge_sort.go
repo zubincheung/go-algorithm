020909fdf70c5aea523aa4e46f1eba0508b7e1f0
Zubin Cheung <zubin@zubin-pc.local>
2018-09-05 10:36:45 +0800

    add：归并排序

--- algorithms/sort/merge_sort.go
+++ algorithms/sort/merge_sort.go
@@ -2,7 +2,7 @@
 
 package sort
 
-// 归并排序
+// 归并排序递归实现
 func MergeSort(list []int) {
 	var tmp []int = make([]int, len(list)) // 初始化一个和list等长的slice
 	mergeGroup(list, tmp, 0, len(list)-1)
@@ -21,7 +21,6 @@ func mergeGroup(list, tmp []int, left, right int) {
 
 // 将有二个有序数列list[left...mid]和list[mid...right]合并
 func merge(list, tmp []int, left, mid, rigth int) {
-
 	i := left    // 左序列指针
 	j := mid + 1 //右序列指针
 	k := left    // 临时列表指针
@@ -31,9 +30,8 @@ func merge(list, tmp []int, left, mid, rigth int) {
 		if list[i] < list[j] {
 			tmp[k] = list[i]
 			i++
-
 		} else {
-			tmp[i] = list[j]
+			tmp[k] = list[j]
 			j++
 		}
 		k++
@@ -58,4 +56,37 @@ func merge(list, tmp []int, left, mid, rigth int) {
 		left++
 	}
 
+}
+
+// 归并排序迭代实现
+func MergeSortIteration(list []int) {
+	var tmp []int = make([]int, len(list)) // 初始化一个和list等长的slice
+	i := 1
+	for i < len(list) {
+		mergePass(list, tmp, i)
+		i = 2 * i
+		mergePass(tmp, list, i)
+		i = 2 * i
+	}
+}
+
+// list中相邻长度为size的子序列两两合并到tmp
+func mergePass(arr, tmp []int, size int) {
+	l := len(arr)
+	i := 0
+	for i < l-2*size+1 {
+		// 两两归并
+		merge(arr, tmp, i, i+size-1, i+2*size-1)
+		i = i + 2*size
+	}
+
+	// 归并最后两个序列
+	if i < l-size+1 {
+		merge(arr, tmp, i, i+size-1, l-1)
+	} else {
+		// 若最后只剩下单个子序列
+		for j := i; j < l; j++ {
+			tmp[j] = arr[j]
+		}
+	}
 }
--- algorithms/sort/merge_sort_test.go
+++ algorithms/sort/merge_sort_test.go
@@ -2,7 +2,9 @@
 
 package sort
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMergeSort(t *testing.T) {
 	type args struct {
@@ -20,12 +22,51 @@ func TestMergeSort(t *testing.T) {
 				want: []int{1, 3, 5, 6, 8, 9},
 			},
 		},
+		{
+			args: args{
+				list: []int{6, 3, 11, 1, 8, 5, 9},
+				want: []int{1, 3, 5, 6, 8, 9, 11},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			MergeSort(tt.args.list)
 			if !SliceEquals(tt.args.list, tt.args.want) {
 
+				t.Error("sort error", tt.args.list)
+			}
+		})
+	}
+}
+
+func TestMergeSortIteration(t *testing.T) {
+	type args struct {
+		list []int
+		want []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			args: args{
+				list: []int{6, 3, 1, 8, 5, 9},
+				want: []int{1, 3, 5, 6, 8, 9},
+			},
+		},
+		{
+			args: args{
+				list: []int{6, 3, 11, 1, 8, 5, 9},
+				want: []int{1, 3, 5, 6, 8, 9, 11},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			MergeSortIteration(tt.args.list)
+			if !SliceEquals(tt.args.list, tt.args.want) {
 				t.Error("sort error", tt.args.list)
 			}
 		})

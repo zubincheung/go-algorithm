972d3fc8c2894ddc375b740f7e57bb26d50faafd
Zubin Cheung <zubin@zubin-pc.local>
2018-09-05 12:19:58 +0800

    add：快速排序迭代实现

---
+++ algorithms/sort/quick_sort.go
@@ -0,0 +1,36 @@
+// Create by Zubin on 2018-09-05 19:00:11
+package sort
+
+// 快速排序,迭代实现
+func QuickSort(list []int) {
+	qSort(list, 0, len(list)-1)
+}
+
+func qSort(list []int, low, hight int) {
+	if low < hight {
+		pivot := partition(list, low, hight)
+
+		qSort(list, low, pivot-1)   // 对低子表递归排序
+		qSort(list, pivot+1, hight) // 对高子表递归排序
+	}
+}
+
+// 将list一分为二，并返回枢轴值
+func partition(list []int, low, hight int) int {
+	pivotKey := list[low]
+
+	for low < hight {
+		//把小于pivotKey的交换到低端
+		for low < hight && list[hight] > pivotKey {
+			hight--
+		}
+		swap(list, low, hight)
+		//把小于pivotKey的交换到低端
+		for low < hight && list[low] < pivotKey {
+			low++
+		}
+		swap(list, low, hight)
+	}
+
+	return low
+}
---
+++ algorithms/sort/quick_sort_test.go
@@ -0,0 +1,37 @@
+// Create by Zubin on 2018-09-05 19:00:11
+package sort
+
+import "testing"
+
+func TestQuickSort(t *testing.T) {
+	type args struct {
+		list []int
+		want []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			args: args{
+				list: []int{6, 3, 1, 8, 5, 9},
+				want: []int{1, 3, 5, 6, 8, 9},
+			},
+		},
+		{
+			args: args{
+				list: []int{6, 3, 11, 1, 8, 5, 9},
+				want: []int{1, 3, 5, 6, 8, 9, 11},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			QuickSort(tt.args.list)
+			if !SliceEquals(tt.args.list, tt.args.want) {
+				t.Error("sort error", tt.args.list)
+			}
+		})
+	}
+}

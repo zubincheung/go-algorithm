3327e5c9197e17c6793d8d6b8070f606da93a37f
Zubin <zubincheung@outlook.com>
2019-03-28 14:48:50 +0800

    add：util

---
+++ util/util.go
@@ -0,0 +1,13 @@
+package util
+
+import (
+	"fmt"
+	"log"
+)
+
+func FailOnErr(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s:%s", msg, err)
+		panic(fmt.Sprintf("%s:%s", msg, err))
+	}
+}
